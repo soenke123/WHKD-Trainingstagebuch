bb7042439affa5d3faf7f702744a1107bef8e271
Sönke Birkner <snke@hotmail.de>
2026-07-27 17:02:34 +0200

    fix

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Trage hier die Werte aus deinem Supabase-Projekt ein
 // (Project Settings → API → URL bzw. anon public key).
-window.SUPABASE_URL      = "https://inzshlmhantrktvlczty.supabase.co/rest/v1/";
+window.SUPABASE_URL      = "https://inzshlmhantrktvlczty.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluenNobG1oYW50cmt0dmxjenR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNTc4ODEsImV4cCI6MjEwMDczMzg4MX0.n2nOvvd3hMtSqH5sbhKTNt9epOAEFHD-zN2QvJ9dySw";
